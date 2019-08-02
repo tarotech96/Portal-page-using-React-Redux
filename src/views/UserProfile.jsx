9e7ffda8980ecd52fea0cf0e6de20ec0d812dc5d
nvcong <nvcong@cmc.com.vn>
2019-08-02 14:37:38 +0700

    template admin react

---
+++ src/views/UserProfile.jsx
@@ -0,0 +1,190 @@
+/*!
+
+=========================================================
+* Light Bootstrap Dashboard React - v1.3.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React, { Component } from "react";
+import {
+  Grid,
+  Row,
+  Col,
+  FormGroup,
+  ControlLabel,
+  FormControl
+} from "react-bootstrap";
+
+import { Card } from "components/Card/Card.jsx";
+import { FormInputs } from "components/FormInputs/FormInputs.jsx";
+import { UserCard } from "components/UserCard/UserCard.jsx";
+import Button from "components/CustomButton/CustomButton.jsx";
+
+import avatar from "assets/img/jisoo3.jpg";
+
+class UserProfile extends Component {
+  render() {
+    return (
+      <div className="content">
+        <Grid fluid>
+          <Row>
+            <Col md={8}>
+              <Card
+                title="Edit Profile"
+                content={
+                  <form>
+                    <FormInputs
+                      ncols={["col-md-5", "col-md-3", "col-md-4"]}
+                      properties={[
+                        {
+                          label: "Company",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Company",
+                          defaultValue: "Start-up",
+                          disabled: true
+                        },
+                        {
+                          label: "Username",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Username",
+                          defaultValue: "taroopa",
+                          disabled: true
+                        },
+                        {
+                          label: "Email",
+                          type: "email",
+                          bsClass: "form-control",
+                          placeholder: "Email",
+                          defaultValue: "tarooppa@gmail.com",
+                          disabled: true
+                        }
+                      ]}
+                    />
+                    <FormInputs
+                      ncols={["col-md-6", "col-md-6"]}
+                      properties={[
+                        {
+                          label: "First name",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "First name",
+                          defaultValue: "Taro"
+                        },
+                        {
+                          label: "Last name",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Last name",
+                          defaultValue: "Oppa"
+                        }
+                      ]}
+                    />
+                    <FormInputs
+                      ncols={["col-md-12"]}
+                      properties={[
+                        {
+                          label: "Adress",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Home Adress",
+                          defaultValue:
+                            "Ha Noi, Viet Nam"
+                        }
+                      ]}
+                    />
+                    <FormInputs
+                      ncols={["col-md-4", "col-md-4", "col-md-4"]}
+                      properties={[
+                        {
+                          label: "City",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "City",
+                          defaultValue: "Ha Noi"
+                        },
+                        {
+                          label: "Country",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Country",
+                          defaultValue: "Viet Nam"
+                        },
+                        {
+                          label: "Postal Code",
+                          type: "number",
+                          bsClass: "form-control",
+                          placeholder: "ZIP Code"
+                        }
+                      ]}
+                    />
+
+                    <Row>
+                      <Col md={12}>
+                        <FormGroup controlId="formControlsTextarea">
+                          <ControlLabel>About Me</ControlLabel>
+                          <FormControl
+                            rows="5"
+                            componentClass="textarea"
+                            bsClass="form-control"
+                            placeholder="Here can be your description"
+                            defaultValue="I like reading book, playing sports,learning programing language"
+                          />
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <Button bsStyle="info" pullRight fill type="submit">
+                      Update Profile
+                    </Button>
+                    <div className="clearfix" />
+                  </form>
+                }
+              />
+            </Col>
+            <Col md={4}>
+              <UserCard
+                bgImage="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400"
+                avatar={avatar}
+                name="Taro Oppa"
+                userName="tarooppa"
+                description={
+                  <span>
+                    "I'm Sofware Engineer
+                    <br />
+                    I like coding"
+                  </span>
+                }
+                socials={
+                  <div>
+                    <Button simple>
+                      <i className="fa fa-facebook-square" />
+                    </Button>
+                    <Button simple>
+                      <i className="fa fa-twitter" />
+                    </Button>
+                    <Button simple>
+                      <i className="fa fa-google-plus-square" />
+                    </Button>
+                  </div>
+                }
+              />
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
